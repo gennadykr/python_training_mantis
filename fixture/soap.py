dedca2397460ee481788dc1f2fa38f8027d442bb
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-06-25 00:54:53 +0300

    Задание №26 -fixed
    SOAP URL is in configuration file now

--- fixture/soap.py
+++ fixture/soap.py
@@ -10,7 +10,7 @@ class SoapHelper:
     def get_project_list(self):
         username = self.app.config["webadmin"]["username"]
         password = self.app.config["webadmin"]["password"]
-        client = Client("http://localhost/mantisbt-2.5.1/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config["soap"]["soap_url"])
         return self.convert_projects_to_model(client.service.mc_projects_get_user_accessible(username, password))
 
     def convert_projects_to_model(self,projects):
